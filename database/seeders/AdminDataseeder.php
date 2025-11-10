344727a78516ab62172c1df9ad5b05f9ef763be8
ali ahmadi <aliahmadi1999001378@gmail.com>
2025-11-10 10:54:17 +0330

    feat(auth): add authentication scaffolding using Laravel Breeze

--- database/seeders/AdminDataseeder.php
+++ database/seeders/AdminDataseeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminDataseeder extends Seeder
 {
@@ -12,6 +14,13 @@ class AdminDataseeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+
+
+            'username' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make(111),
+
+        ]);
     }
 }
