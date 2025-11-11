f386df895a2081ccbd3d153900c5b9b70ed9624b
ali ahmadi <aliahmadi1999001378@gmail.com>
2025-11-11 11:46:44 +0330

    feat: customize login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,114 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="NobleUI">
+	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-        <!-- Username Address -->
-        <div>
-            <x-input-label for="username" :value="__('username')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
+	<title>NobleUI - HTML Bootstrap 5 Admin Dashboard Template</title>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+  <!-- End fonts -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+	<!-- core:css -->
+	<link rel="stylesheet" href="{{ asset('../../../backend/assets/vendors/core/core.css') }}">
+	<!-- endinject -->
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+	<!-- Plugin css for this page -->
+	<!-- End plugin css for this page -->
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('../../../backend/assets/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('../../../backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<!-- endinject -->
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="{{ asset('../../../backend/assets/css/demo2/style.css') }}">
+  <!-- End layout styles -->
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+  <link rel="shortcut icon" href="{{ asset('../../../backend/assets/images/favicon.png') }}" />
+  <style>
+    .auth-login-side-wrapper{
+      background-image: url('../../../backend/assets/images/login.png');
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+</head>
+<body>
+	<div class="main-wrapper">
+		<div class="page-wrapper full-page">
+			<div class="page-content d-flex align-items-center justify-content-center">
+
+				<div class="row w-100 mx-0 auth-page">
+					<div class="col-md-8 col-xl-6 mx-auto">
+						<div class="card">
+							<div class="row">
+                <div class="col-md-4 pe-md-0">
+                  <div class="auth-login-side-wrapper">
+
+                  </div>
+                </div>
+                <div class="col-md-8 ps-md-0">
+                  <div class="auth-form-wrapper px-4 py-5">
+                    <a href="#" class="noble-ui-logo logo-light d-block mb-2">ام‌جی-<span>نمادی</span></a>
+                   <h5 class="text-muted fw-normal mb-4">خوش آمدید! وارد حساب کاربری خود شوید.</h5>
+
+                    <form method="POST" action="{{ route('login') }}" class="forms-sample">
+                         @csrf
+                      <div class="mb-3">
+                        <label for="userEmail" class="form-label">نام کاربری </label>
+                        <input type="uesername" class="form-control" name="username" placeholder="نام کاربری ">
+                      </div>
+                      <div class="mb-3">
+                        <label for="userPassword" class="form-label">رمز عبور </label>
+                        <input type="text" class="form-control"  name="password" autocomplete="current-password" placeholder="رمز عبور">
+                      </div>
+          
+                      <div>
+                    
+                        <button type="submit" class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
+                
+                           وارد شدن 
+                        </button>
+                      </div>
+                     <a href="register.html" class="d-block mt-3 text-muted" style="text-decoration: underline;">فراموشی رمز عبور</a>
+
+                    </form>
+
+
+                  </div>
+                </div>
+              </div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- core:js -->
+	<script src="{{ asset('../../../backend/assets/vendors/core/core.js') }}"></script>
+	<!-- endinject -->
+
+	<!-- Plugin js for this page -->
+	<!-- End plugin js for this page -->
+
+	<!-- inject:js -->
+	<script src="{{ asset('../../../backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+	<script src="{{ asset('../../../backend/assets/js/template.jsbackend/') }}"></script>
+	<!-- endinject -->
+
+	<!-- Custom js for this page -->
+	<!-- End custom js for this page -->
+
+</body>
+</html>
