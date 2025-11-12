9ae86fe52a7110656d02fa2354e06f1bcbbf7e06
ali ahmadi <aliahmadi1999001378@gmail.com>
2025-11-12 13:16:47 +0330

    fix(admin-profile): edit section 2 layout and input handling

--- resources/views/admin/pages/edit_profile.blade.php
+++ resources/views/admin/pages/edit_profile.blade.php
@@ -1,11 +1,46 @@
-
-
-
-
 @extends('admin.admin_dashboard')
 
-
-
 @section('admin')
- <h2> ویرایش پروفایل </h2>
+
+<div class="col-md-12 grid-margin stretch-card" dir="rtl">
+  <div class="card">
+    <div class="card-body">
+
+      <h6 class="card-title">ویرایش پروفایل</h6>
+
+      <form class="forms-sample"> 
+        <div class="row mb-3">
+          <label for="exampleInputUsername2" class="col-sm-3 col-form-label">نام کاربری</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control text-end" value="{{ $admin->username }}" placeholder="نام کاربری" style="direction: rtl;">
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="exampleInputEmail2" class="col-sm-3 col-form-label">ایمیل</label>
+          <div class="col-sm-9">
+            <input type="email" class="form-control text-end" value="{{ $admin->email }}" autocomplete="off" placeholder="ایمیل" style="direction: rtl;">
+          </div>
+        </div>
+
+          <div class="row mb-3">
+          <label for="exampleInputEmail2" class="col-sm-3 col-form-label">عکس پروفایل </label>
+          <div class="col-sm-9">
+            <input type="file" class="form-control text-end" id="exampleInputFile" name="profile_image" style="direction: rtl;">
+          </div>
+        </div>
+
+          <div class="row mb-3">
+          <label for="exampleInputEmail2" class="col-sm-3 col-form-label"></label>
+          <div class="col-sm-9">
+           <img src="{{ asset('upload/my-profile-img.jpg') }}" alt="" style="width:90px; height:90px;">
+          </div>
+        </div>
+
+        <button class="btn btn-secondary">ویرایش</button>
+      </form>
+
+    </div>
+  </div>
+</div>
+
 @endsection
